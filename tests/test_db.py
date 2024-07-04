390141c795341cb6e4e19e5df09fac0eb790cd32
Joao Gabriel <joao.gabriel156@hotmail.com>
2024-07-03 22:30:58 -0300

    feat: add sqlalchemy+fixture+migrations config

---
+++ tests/test_db.py
@@ -0,0 +1,15 @@
+from sqlalchemy import select
+from sqlalchemy.orm.session import Session
+
+from fast_api.models import User
+
+
+def test_create_user(session: Session):
+    new_user = User(
+        username="alice", password="secret", email="teste@test.com"
+    )
+    session.add(new_user)
+    session.commit()
+
+    user = session.scalar(select(User).where(User.username == "alice"))
+    assert user.username == "alice"
